9cccce29658e39bdaa4243ff527b091b8bb6e937
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-04-24 10:42:36 -0300

    Create route Appointments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import appointmentRouter from './appointment.routes';
 
 const routes = Router();
+routes.use('/appointments', appointmentRouter);
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express, { json } from 'express';
+import express from 'express';
 import routes from './routes';
 
 const app = express();
 
-app.use(routes);
-
 app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('Server started on port 3333');
