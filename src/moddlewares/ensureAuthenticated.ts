89566d4602c166efbce7580b319011103e311156
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-04-26 23:08:06 -0300

    Using AppError in services and routes

--- src/moddlewares/ensureAuthenticated.ts
+++ src/moddlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 interface TokenPayload {
   iat: number;
@@ -32,6 +33,6 @@ export default function ensureAuthenticated(
 
     return next();
   } catch (error) {
-    throw new Error('Invalid JWT token');
+    throw new AppError('Invalid JWT token', 401);
   }
 }
