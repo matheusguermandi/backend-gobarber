68e2304af10aed96a86ff199c7cd69d534698fe7
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-05-10 20:13:02 -0300

    Construindo Migration UserToken

--- src/shared/infra/typeorm/migrations/1589151271338-CreateUsersTokens.ts
+++ src/shared/infra/typeorm/migrations/1589151271338-CreateUsersTokens.ts
@@ -1,11 +1,56 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUsersTokens1589151271338 implements MigrationInterface {
+export default class CreateUsersTokens1589151271338
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_token',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'token',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'TokenUserFK',
+            columnNames: ['user_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<any> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_token');
+    await queryRunner.dropForeignKey('user_token', 'TokenUserFK');
+  }
 }
