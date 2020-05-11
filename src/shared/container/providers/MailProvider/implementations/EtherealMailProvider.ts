92ea66f3a10c4c2ae63bd352cba5f4d0e02e6170
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-05-11 15:41:33 -0300

    Configurando provider Ethereal

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,5 +1,25 @@
+import nodemailer, { Transporter } from 'nodemailer';
+
 import IMailProvider from '../models/IMailProvider';
 
 export default class EtherealMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    nodemailer.createTestAccount().then(account => {
+      const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+
+      this.client = transporter;
+    });
+  }
+
   public async sendMail(to: string, body: string): Promise<void> {}
 }
