a41dcbcd79e58708954c85eafad4452d79805adb
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-05-14 20:15:40 -0300

    Atualizando provider fake mailtemplate

--- src/shared/container/providers/MailTemplateProvider/fake/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fake/FakeMailTemplateProvider.ts
@@ -1,8 +1,7 @@
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
-import IParseEmailTemplateDTO from '../dtos/IParseEmailTemplateDTO';
 
 export default class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({ template }: IParseEmailTemplateDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
