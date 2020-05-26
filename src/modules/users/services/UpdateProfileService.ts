bf4009e463725ddd97a00461fb4e47d36d6d2bef
matheusguermandi <matheus_guermandi@hotmail.com>
2020-05-25 23:12:13 -0300

    Construindo service UpdateProfile

---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,30 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ user_id, email, name }: IRequest): Promise<void> {}
+}
+
+export default UpdateProfileService;
