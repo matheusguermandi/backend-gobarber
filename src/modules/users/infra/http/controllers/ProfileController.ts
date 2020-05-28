cff93c1947d317b9611777d266c247a7182cbd37
matheusguermandi <matheus_guermandi@hotmail.com>
2020-05-27 21:58:44 -0300

    Adicionando método Show no controller de Profil

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -4,6 +4,10 @@ import { container } from 'tsyringe';
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 
 export default class ProfileController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    // exibição do perfil
+  }
+
   public async update(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const { name, email, old_password, password } = request.body;
