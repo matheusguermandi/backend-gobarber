878e3f004bf7d2186b4b47e036ea65e5815d8058
matheusguermandi <matheus_guermandi@hotmail.com>
2020-05-29 22:27:02 -0300

    Atualizando FakeUserRepository

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -2,6 +2,7 @@ import User from '@modules/users/infra/typeorm/entities/User';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 export default interface IUserRepository {
+  findAllProviders(except_user_id?: string): Promise<User[]>;
   findByEmail(email: string): Promise<User | undefined>;
   findById(id: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
