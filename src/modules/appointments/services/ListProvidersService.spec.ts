cd67c50ecc36303f86124ed0a6241de44b15a548
matheusguermandi <matheus_guermandi@hotmail.com>
2020-05-29 22:52:27 -0300

    Construindo teste para ListProviderService

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list the providers', async () => {
+    const user01 = await fakeUsersRepository.create({
+      name: 'Matheus01',
+      email: 'matheus_guermandi@hotmail.com',
+      password: '123',
+    });
+
+    const user02 = await fakeUsersRepository.create({
+      name: 'Matheus02',
+      email: 'matheus_guermandi@hotmail.com02',
+      password: '123',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'Matheus03',
+      email: 'matheus_guermandi@hotmail.com03',
+      password: '123',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toBe([user01, user02]);
+  });
+});
