65b949a0f886dfa4d0747356ad7e5efa2ae8de70
matheusguermandi <matheus_guermandi@hotmail.com>
2020-06-02 16:11:44 -0300

    Construindo listdayavailability Service

---
+++ src/modules/appointments/services/ListDayAvailabilityService.ts
@@ -0,0 +1,35 @@
+import { injectable, inject } from 'tsyringe';
+// import { getDaysInMonth, getDate } from 'date-fns';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+  day: number;
+}
+
+type IResponse = Array<{
+  hour: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListDayAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IRequest): Promise<IResponse> {
+    return [{ hour: 8, available: true }];
+  }
+}
+
+export default ListDayAvailabilityService;
