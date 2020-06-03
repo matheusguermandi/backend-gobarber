36b6f3f63fdabc1c775aabf65a8a3d95ae5ac368
matheusguermandi <matheus_guermandi@hotmail.com>
2020-06-03 16:28:30 -0300

    Criando migration para adduseridtoappointments

---
+++ src/shared/infra/typeorm/migrations/1591212473661-AddUserIdToAppointments.ts
@@ -0,0 +1,8 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddUserIdToAppointments1591212473661
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {}
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
