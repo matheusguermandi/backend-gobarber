742c697d906f3229cbfe376c28bd27f6cde4b35e
matheusguermandi <matheus_guermandi@hotmail.com>
2020-06-03 15:55:38 -0300

    Atualizando listdayavailability Service

--- src/modules/appointments/services/ListDayAvailabilityService.ts
+++ src/modules/appointments/services/ListDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -49,9 +49,12 @@ class ListDayAvailabilityService {
         appointment => getHours(appointment.date) === hour,
       );
 
+      const currentDate = new Date(Date.now());
+      const compareDate = new Date(year, month, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
 
