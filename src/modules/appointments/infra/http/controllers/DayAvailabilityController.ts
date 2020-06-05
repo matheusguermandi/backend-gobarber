69b72477d163bc5d88455b31e29ffe616923acfe
matheusguermandi <matheus_guermandi@hotmail.com>
2020-06-05 14:44:50 -0300

    Construindo controllers de month e day availability

---
+++ src/modules/appointments/infra/http/controllers/DayAvailabilityController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListDayAvailabilityService from '@modules/appointments/services/ListDayAvailabilityService';
+
+export default class MonthAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id, month, year, day } = request.body;
+
+    const listDayAvailability = container.resolve(ListDayAvailabilityService);
+
+    const availability = await listDayAvailability.execute({
+      provider_id,
+      month,
+      year,
+      day,
+    });
+
+    return response.json(availability);
+  }
+}
