24d7907b48891e2faa858d9286f0947dde865015
matheusguermandi <matheus_guermandi@hotmail.com>
2020-06-05 14:50:58 -0300

    Atualizando rota de provider

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,21 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import DayAvailabilityController from '@modules/appointments/infra/http/controllers/DayAvailabilityController';
+import MonthAvailabilityController from '@modules/appointments/infra/http/controllers/MonthAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const dayAvailabilityController = new DayAvailabilityController();
+const monthAvailabilityController = new MonthAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
-providersRouter.post('/', providersController.index);
+providersRouter.get('/', providersController.index);
+providersRouter.get('/:id/day-availability', dayAvailabilityController.index);
+providersRouter.get(
+  '/:id/month-availability',
+  monthAvailabilityController.index,
+);
 
 export default providersRouter;
