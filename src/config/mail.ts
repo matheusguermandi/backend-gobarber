933504ad4b92f57f6b95fe13155a0d79c0b68e2e
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-06-11 23:38:07 -0300

    Atualizando config mail

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,7 +1,21 @@
 interface IMailConfig {
   driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
 }
 
 export default {
   driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'contato@matheusguermandi.com.br',
+      name: 'Matheus Guermandi',
+    },
+  },
 } as IMailConfig;
