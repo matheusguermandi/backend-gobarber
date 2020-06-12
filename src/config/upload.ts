991f6bba032ec8057522951e3fdcf08252719ee9
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-06-12 13:14:18 -0300

    Atualizando config de uploads

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,21 +1,37 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 const uploadsFolder = path.resolve(tmpFolder, 'uploads');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  config: {
+    disk: {
+      storage: StorageEngine;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder,
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
+  config: {
+    disk: {
+      storage: multer.diskStorage({
+        destination: tmpFolder,
+        filename(request, file, callback) {
+          const fileHash = crypto.randomBytes(10).toString('HEX');
+          const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
+          return callback(null, fileName);
+        },
+      }),
     },
-  }),
+  },
 };
