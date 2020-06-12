5b2306ebf5c1fb21d548e6e015bffef956f5a593
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-06-12 13:07:23 -0300

    Criando provader S3Storage

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -3,11 +3,11 @@ import { container } from 'tsyringe';
 import IStorageProvider from './models/IStorageProvider';
 
 import DiskStorageProvider from './implementations/DiskStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 
 const provider = {
   diskStorage: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  provider.diskStorage,
-);
+
+container.registerSingleton<IStorageProvider>('StorageProvider', provider.s3);
