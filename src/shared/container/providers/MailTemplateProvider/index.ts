597979b9e6b5b13da3cb559998e4a1afc8f82af5
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-06-12 00:26:32 -0300

    Organizando o container

---
+++ src/shared/container/providers/MailTemplateProvider/index.ts
@@ -0,0 +1,14 @@
+import { container } from 'tsyringe';
+
+import IMailTemplateProvider from './models/IMailTemplateProvider';
+
+import HandlebarsMailTemplateProvider from './implementations/HandlebarsMailTemplateProvider';
+
+const provider = {
+  handlebars: HandlebarsMailTemplateProvider,
+};
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  provider.handlebars,
+);
